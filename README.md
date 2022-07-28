b7d39357890a497057b0d523b6bab1edb6448eb8
nimoc <nimocc@126.com>
2022-07-28 20:02:24 +0800

    Register

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # pprof
- 
+
+> [goclub/http](https://github.com/goclub/http) 的 pprof 分析工具
+
+```go
+package main
+
+import xhttp "github.com/goclub/http"
+import xpprof "github.com/goclub/pprof"
+import log "log"
+import http "net/http"
+
+func main () {
+    r := xhttp.NewRouter(xhttp.RouterOption{})
+    server := &http.Server{
+        Addr: ":5241",
+        Handler: r,
+    }
+    xpprof.Register(r.NetHttpHandleFunc)
+	log.Print("http://localhost" + server.Addr + "/debug/")
+	log.Printf("%+v", server.ListenAndServe())
+}
+```
---
+++ pprof.go
@@ -0,0 +1,25 @@
+package xpprof
+
+import (
+	"net/http"
+	"net/http/pprof"
+)
+
+func Register(handleFunc func(path string, handler func(w http.ResponseWriter, r *http.Request))) {
+	handleFunc("/debug/", pprof.Index)
+	handleFunc("/debug/cmdline", pprof.Cmdline)
+	handleFunc("/debug/profile", pprof.Profile)
+	handleFunc("/debug/symbol", pprof.Symbol)
+	handleFunc("/debug/trace", pprof.Trace)
+	handleFunc("/debug/allocs", wrapHandler(pprof.Handler("allocs")))
+	handleFunc("/debug/block", wrapHandler(pprof.Handler("block")))
+	handleFunc("/debug/goroutine", wrapHandler(pprof.Handler("goroutine")))
+	handleFunc("/debug/heap", wrapHandler(pprof.Handler("heap")))
+	handleFunc("/debug/mutex", wrapHandler(pprof.Handler("mutex")))
+	handleFunc("/debug/threadcreate", wrapHandler(pprof.Handler("threadcreate")))
+}
+func wrapHandler(h http.Handler) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		h.ServeHTTP(w, r)
+	}
+}
